e18b034fc8e6a3c68280ea288b21db9f030af65b
Dejailson Silva <kauakarate@gmail.com>
2020-09-07 13:32:53 -0300

    get,get(int id) e estrutura das pastas feitas

---
+++ Controllers/RacaController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API_Pet.Domains;
+using API_Pet.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API_Pet.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RacaController : ControllerBase
+    {
+        RacaRepository rep = new RacaRepository();
+
+        // GET: api/<RacaController>
+        [HttpGet]
+        public List<Raca> Get()
+        {
+            return rep.LerTodos();
+        }
+
+        // GET api/<RacaController>/5
+        [HttpGet("{id}")]
+        public Raca Get(int id)
+        {
+            return rep.BuscarPorId(id);
+        }
+
+        // POST api/<RacaController>
+        [HttpPost]
+        public Raca Post([FromBody] Raca r)
+        {
+            return rep.Cadastrar(r);
+        }
+
+        // PUT api/<RacaController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<RacaController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Interfaces/IRaca.cs
@@ -0,0 +1,26 @@
+﻿using API_Pet.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Pet.Interfaces
+{
+    interface IRaca
+    {
+        //Fazemos os metodos de CRUD e Realizamos uma busca por Id
+        //CREATE
+        Raca Cadastrar(Raca t);
+
+        //READ
+        List<Raca> LerTodos();
+
+        //UPDATE
+        Raca Alterar(int id, Raca t);
+
+        //DELETE
+        void Excluir(int id);
+
+        Raca BuscarPorId(int id);
+    }
+}
